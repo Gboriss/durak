d67be1665660b10e10a63dfa5238bd07936c34a2
Galina <galinaborissenko@gmail.com>
2020-07-28 13:59:47 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,70 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+<div class="app">
+	<div class="carts">
+		<div class="compCarts">
+			<p>карты компьютера</p>
+			<ActCards />
+		</div>
+
+		<Bit />
+
+		<div class="myCarts">
+			<p>Ваши карты</p>
+			<PlCards />
+		</div>
+	</div>
+	<div class="deck">
+		<p>24</p>
+		<img class="deckImg" src="./assets/images/13_2.jpg" alt="">	
+	</div>
+</div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import PlCards from '@/components/PlCards.vue'
+import ActCards from '@/components/ActCards.vue'
+import Bit from '@/components/Bit.vue'
 
-#nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+export default {
+	name: 'App',
+  	components: {
+	PlCards,
+	ActCards,
+	Bit
   }
+
 }
+</script>
+
+<style lang="scss">
+@import './styles/reset.css';
+@import './styles/fonts.css';
+
+.app {
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	background: #233f23;
+    box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.05);
+	// width: 1280px;
+	max-width: 1280px;
+	
+	margin: 20px auto;
+}
+
+.carts {
+	width: 550px;
+	max-width: 550px;
+	padding: 20px;
+
+}
+
+.deckImg {
+	padding: 0 20px;
+
+}
+
+
+
 </style>
---
+++ src/components/PlCards.vue
@@ -0,0 +1,26 @@
+<template>
+   <div>
+        <img class="component" src="../assets/images/14_0.jpg">
+        <img class="component" src="../assets/images/6_2.jpg">
+        <img class="component" src="../assets/images/7_3.jpg">
+        <img class="component" src="../assets/images/14_2.jpg">
+        <img class="component" src="../assets/images/13_1.jpg">
+        <img class="component" src="../assets/images/12_2.jpg">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PlCards',
+}
+</script>
+
+<style lang="scss" scoped>
+
+div {
+    display: flex;
+	// justify-content: center;
+}
+
+
+</style>
